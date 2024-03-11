c4258fc4f0cbdecf819dd44f996d100199dabab2
Siddhartha <siddharthapaul101@gmail.com>
2024-03-11 22:55:57 +0530

    changed the emp id and dept id formata and fixed unit tests

--- src/main/java/com/department/constant/DepartmentConstant.java
+++ src/main/java/com/department/constant/DepartmentConstant.java
@@ -8,11 +8,11 @@ public class DepartmentConstant {
 
     public static final String DEPT_ID_MSG = "Department ID cannot be empty";
 
-    public static final String DEPT_ID_REGEX_MSG = "Department ID should start with D followed by 2 digits";
+    public static final String DEPT_ID_REGEX_MSG = "Department ID should start with DID followed by 1 or 2 digit(s)";
 
-    public static final String EMP_ID_REGEX_MSG = "Employee ID should start with E followed by 5 digits";
+    public static final String EMP_ID_REGEX_MSG = "Employee ID should start with EID followed by digit(s)";
 
-    public static final String EMP_ID_REGEXP = "^[E][0-9]{5}$";
+    public static final String EMP_ID_REGEXP = "^EID[0-9]+";
 
-    public static final String DEPT_ID_REGEXP = "^[D][0][1-5]{1}$";
+    public static final String DEPT_ID_REGEXP = "^DID[0-9]{1,2}$";
 }
